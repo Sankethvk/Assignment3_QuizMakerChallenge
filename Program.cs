ae8c96aec45a048a4f47f107fc80996a176dab24
SanketV <sankethvivek@gmail.com>
2021-10-11 12:12:46 +0530

    Quiz Maker Callenge including reading from file

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizMakerChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            quiz.quiz_file();
+            
+           
+        }
+    }
+}
